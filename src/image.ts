714c9ce584007507e314d26476d31b09dcdb93eb
Fred Vernier <frederic.vernier@gmail.com>
2018-03-06 13:19:21 +0100

    add missin files

--- src/image.ts
+++ src/image.ts
@@ -9,9 +9,9 @@ export default class Image {
     }
 
     fillByTile(color:Color, tile:Tile, mask?:Mask) {
-        for(let r = tile.y; r < tile.y + tile.mask.height; r++) {
+        for(let r = Math.ceil(tile.y); r < tile.y + tile.mask.height; r++) {
             if(r >= this.height) break;
-            for(let c = tile.x; c < tile.x + tile.mask.width; c++) {
+            for(let c = Math.ceil(tile.x); c < tile.x + tile.mask.width; c++) {
                 if(c >= this.width) break;
                 if(mask && r < mask.height && c < mask.width && mask.mask[r][c] == 0)
                     continue;
@@ -20,6 +20,34 @@ export default class Image {
             }
         }
     }
+
+    fillByShapedTile(color:Color, tile:Tile, mask:Mask|undefined, id:string ) {
+      let canvas:any = document.getElementById(id);
+
+      let ctx = canvas.getContext('2d');
+        for(let r = Math.ceil(tile.y); r < tile.y + tile.mask.height; r++) {
+            if(r >= this.height) break;
+            for(let c = Math.ceil(tile.x); c < tile.x + tile.mask.width; c++) {
+                if(c >= this.width) break;
+                if (ctx.isPointInPath(tile.mask.path, c, r)) {
+                  console.log(r+","+c);
+                  console.log(tile.mask.path);
+                  console.log(ctx.isPointInPath(tile.mask.path, c, r));
+                }
+
+                if (!mask.pols.isPointInPolys(c, r))
+                  continue;
+                //if (!ctx.isPointInPath(tile.mask.path, c, r))
+                //  continue;
+
+                //if(mask && r < mask.height && c < mask.width && mask.mask[r][c] == 0)
+                //   continue;
+
+                this.pixels[r][c] = color;
+            }
+        }
+    }
+
 
     fillByRect(color:Color, rect:Rect) {
       for(let r = rect.min.y; r < rect.max.y; r++) {
