25cd0a5aa69bb5fb669b53da9d382cfb3d11c333
Fred Vernier <frederic.vernier@gmail.com>
2018-03-06 13:24:03 +0100

    ng

--- src/fred-test.ts
+++ src/fred-test.ts
@@ -1,18 +1,60 @@
 // compile/transpile with tsc src/fred-test.ts --outfile dist/fred-test.js
 
-class Student {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+// found pngjs here :
+// https://www.npmjs.com/package/pngjs
+// found types for this lib here :
+// https://www.npmjs.com/package/@types/pngjs
+
+import * as PNG from "pngjs";
+
+class Poly {
+  ptx:number[]=[];
+  pty:number[]=[];
+}
+
+export default class Polys2D {
+    public nbpolys: number =0;
+    allpolys:Poly[]=[];
+
+    constructor(public name: string) {
+    }
+
+    addPoly(ptx:number[], pty:number[]){
+      let pol:Poly = new Poly();
+      pol.ptx = ptx;
+      pol.pty = pty;
+      this.allpolys.push(pol);
+    }
+
+    isPointInPolys(x:number, y:number){
+      // inspired from
+      //+ Jonas Raoni Soares Silva
+      //@ http://jsfromhell.com/math/is-point-in-poly [rev. #0]
+
+      //console.log("isPointInPoly "+pt.x+","+pt.y);
+      let res:boolean=false;
+      for (let k in this.allpolys){
+        let poly:Poly = this.allpolys[k];
+        let i:number = -1;
+        let l:number = poly.ptx.length;
+        let j:number = l - 1;
+        for(; ++i < l; j = i){
+          ((poly.pty[i] <= y && y < poly.pty[j]) || (poly.pty[j] <= y && y < poly.pty[i]))
+          && (x < (poly.ptx[j] - poly.ptx[i]) * (y - poly.pty[i]) / (poly.pty[j] - poly.pty[i]) + poly.ptx[i])
+          && (res = !res);
+        }
+      }
+      return res;
     }
 }
+
 
 interface Person {
     firstName: string;
     lastName: string;
 }
 
-function greeter(person : Person) {
-    return "Bonjour, " + person.firstName + " " + person.lastName;
-}
+
+
+
 
