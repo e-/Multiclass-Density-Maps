c6bac1abcad54b950cb4bc0a8734b47871b19ea9
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2018-03-13 06:51:39 +0100

    Push my tdigest.d.ts, sorry...

--- src/tdigest.d.ts
+++ src/tdigest.d.ts
@@ -1,14 +1,43 @@
-declare module 'tdigest' {
-    export class Digest {
-        push(ele: number):void;
-        push(ele: number[]):void;
+declare module "tdigest" {
 
-        compress():void;
-
-        percentile(p: number):number;
-        percentile(p: number[]):number[];
-
-        p_rank(x: number):number;
-        p_rank(x: number[]):number[];
+    interface Centroid {
+        mean: number;
+        n: number;
     }
+
+    class TDigest {
+        constructor(delta?:number|false, K?: number, CX?: number);
+        
+        reset();
+        size(): number;
+        toArray(): Centroid[];
+        summary(): string;
+        push(x: number);
+        push(x: number[], n?: number);
+        push_centroid(c:Centroid|Centroid[]);
+        find_nearest(x:number): Centroid;
+        bound_mean(x: number): [number, number];
+        p_rank(x: number): number;
+        p_rank(x: number[]): number[];
+        bound_mean_cumn(cumn:number): [number, number];
+        percentile(p:number):number;
+        percentile(p:number[]):number[];
+        compress();
+
+    }
+
+    interface Config {
+        mode?: 'auto'|'disc'|'cont';
+        delta?: number;
+        ratio?: number;
+        thresh?: number;
+    }
+
+    class Digest extends TDigest {
+        constructor(config?:Config);
+        push(x: number);
+        push(x: number[], n?: number);
+        check_continuous(): boolean;
+    }
+
 }
