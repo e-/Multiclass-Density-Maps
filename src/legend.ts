acfa9d048d8a7b6550e79e1254302704d3a2b36f
Fred Vernier <frederic.vernier@gmail.com>
2018-03-21 07:54:35 +0100

    better hatching. spec for it

--- src/legend.ts
+++ src/legend.ts
@@ -190,7 +190,7 @@ function horizontalColormaps(id:string, interp:Interpreter) {
         .data(markerValues)
         .enter()
         .append('line')
-        .attr('x1', 0)
+        .attr('x1', 10)
         .attr('x2', 0)
         .attr('y1', 0)
         .attr('y2', rowHeight)
